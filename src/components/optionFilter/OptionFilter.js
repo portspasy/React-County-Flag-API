737f514b391b3a3c5998af70c343d2eabe461022
Taulant Culi <t.culi91@gmail.com>
2020-09-22 17:52:48 +0200

    creating SearchFilter and OptionFilter Components and SCSS file

---
+++ src/components/optionFilter/OptionFilter.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./OptionFilter.scss";
+
+const OptionFilter = () => {
+  return (
+    <div className="option-filter">
+      <p>Option Filter Comp.</p>
+    </div>
+  );
+};
+
+export default OptionFilter;
---
+++ src/components/searchFilter/SearchFilter.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./SearchFilter.scss";
+
+const SearchFilter = () => {
+  return (
+    <div className="search-filter">
+      <p>Search Filter Comp.</p>
+    </div>
+  );
+};
+
+export default SearchFilter;
